da21eb038161225576161d7cbde0f072cdc42319
HishamBoyMunna <158536380+HishamBoyMunna@users.noreply.github.com>
2025-11-07 11:46:14 +0530

    last safe state

--- python/MinionGame.py
+++ python/MinionGame.py
@@ -7,35 +7,19 @@ def minion_game(string):
     stuart_score = 0
     kevin_score = 0
 
-    def get_kevin_score(string, score):
-        length = len(string)
-        kevin_score = score
-        for i, alpha in enumerate(string):
-            if alpha in vowels:
-                # kevin_score += 1
-                for j in range(1, len(string[i:length]) + 1):
-                    if string[i : i + j] in string:
-                        # print(string[i : i + j])
-                        kevin_score += 1
-        return kevin_score
-
-    kevin_score = get_kevin_score(string, kevin_score)
-    # print(kevin_score)
-
-    def get_stuart_score(string, score):
-        length = len(string)
-        stuart_score = score
-        for i, alpha in enumerate(string):
-            if alpha not in vowels:
-                # kevin_score += 1
-                for j in range(1, len(string[i:length]) + 1):
-                    if string[i : i + j] in string:
-                        # print(string[i : i + j])
-                        stuart_score += 1
-        return stuart_score
-
-    stuart_score = get_stuart_score(string, stuart_score)
-    # print(stuart_score)
+    length = len(string)
+    for i, alpha in enumerate(string):
+        if alpha in vowels:
+            # kevin_score += 1
+            for j in range(1, len(string[i:length]) + 1):
+                if string[i : i + j] in string:
+                    # print(string[i : i + j])
+                    kevin_score += 1
+        elif alpha not in vowels:
+            for j in range(1, len(string[i:length]) + 1):
+                if string[i : i + j] in string:
+                    # print(string[i : i + j])
+                    stuart_score += 1
 
     if kevin_score < stuart_score:
         print(f"Stuart {stuart_score}")
